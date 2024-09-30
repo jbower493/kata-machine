f98982c104d9feab6a065094528f02e1c017f2f6
Jamie Bower <j_bower@hotmail.co.uk>
2024-09-30 21:50:22 +1000

    feat: redo binary search to understand it

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,21 +1,27 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    let low = 0;
-    let high = haystack.length;
+    let lo = 0,
+        hi = haystack.length;
 
-    do {
-        const mid = Math.floor(low + (high - low) / 2);
-        const value = haystack[mid];
+    while (lo < hi) {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
 
-        if (value === needle) {
+        if (v === needle) {
             return true;
-        }
-        if (value > needle) {
-            high = mid;
+        } else if (v < needle) {
+            // search the right half
+
+            // lo = m + 1, because lower bound is inclusive, and we don't want to check m again, so we need the new lo to be m + 1
+            lo = m + 1;
         } else {
-            // High point is exclusive, low point is inclusive, so we add 1 to the low in this case
-            low = mid + 1;
+            // search the left half
+
+            // hi = m, because higher bound is exclusive, by setting high to m (not m + 1), we won't check m again
+            hi = m;
         }
-    } while (low < high);
+    }
 
     return false;
 }
+
+// high is exclusive, low is inclusive
